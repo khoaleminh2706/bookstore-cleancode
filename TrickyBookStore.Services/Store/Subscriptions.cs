26a379164a590e179149647beffe2fa0f6a38c1c
Khoa Le Minh <p3t3r276@gmail.com>
2020-09-28 16:56:19 +0700

    update pricedetails for each sub types

--- TrickyBookStore.Services/Store/Subscriptions.cs
+++ TrickyBookStore.Services/Store/Subscriptions.cs
@@ -10,32 +10,36 @@ namespace TrickyBookStore.Services.Store
             new Subscription { Id = 1, SubscriptionType = SubscriptionTypes.Paid, Priority = ...,
                 PriceDetails = new Dictionary<string, double>
                 {
-                   
+                    { "FixPrice", 50 },
+                    { "Discount", 0.05 },
                 }
             },
             new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = ...,
                 PriceDetails = new Dictionary<string, double>
                 {
-
+                    { "FixPrice", 0 },
+                    { "Discount", 0.1 },
                 }
             },
-            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = ...,
+            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = 1,
                 PriceDetails = new Dictionary<string, double>
                 {
-                   
+                   { "FixPrice", 200 },
+                   { "Discount", 0.15 },
                 }
             },
             new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
                 PriceDetails = new Dictionary<string, double>
                 {
-                  
+                  { "FixPrice", 75 },
+                  { "Discount", 0.15 },
                 }              
             },
             new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
                 PriceDetails = new Dictionary<string, double>
                 {
                     { "FixPrice", 75 },
-                    ....
+                    { "Discount", 0.15 },
                 },
                 BookCategoryId = 1
             },
@@ -43,7 +47,7 @@ namespace TrickyBookStore.Services.Store
                 PriceDetails = new Dictionary<string, double>
                 {
                     { "FixPrice", 75 },
-                    ....
+                    { "Discount", 0.15 },
                 },
                 BookCategoryId = 3
             }
