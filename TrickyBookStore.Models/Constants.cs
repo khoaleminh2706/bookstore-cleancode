805d59bba7dfac9c827a6a435016aa57b40a5886
Khoa Le Minh <p3t3r276@gmail.com>
2020-09-29 15:30:45 +0700

    update sub prices and priority

--- TrickyBookStore.Models/Constants.cs
+++ TrickyBookStore.Models/Constants.cs
@@ -5,8 +5,9 @@
         public static class PriceDetailsType
         {
             public const string FixedPrice = "FixPrice";
-            public const string DiscountAmount = "Discount";
-            public const string DiscountThreshold = "DiscountThreshold";
+            public const string DiscountOldBook = "DiscountOldBook";
+            public const string DiscountNewBook = "DiscountNewBook";
+            public const string DiscountNewBookThreshold = "DiscountNewBookThreshold";
         }
     }
 }
--- TrickyBookStore.Services/Store/Subscriptions.cs
+++ TrickyBookStore.Services/Store/Subscriptions.cs
@@ -12,49 +12,47 @@ namespace TrickyBookStore.Services.Store
                 PriceDetails = new Dictionary<string, double>
                 {
                     { Constants.PriceDetailsType.FixedPrice, 50 },
-                    { Constants.PriceDetailsType.DiscountAmount, 0.05 },
-                    { Constants.PriceDetailsType.DiscountThreshold, 3 },
+                    { Constants.PriceDetailsType.DiscountOldBook, 0.05 },
+                    { Constants.PriceDetailsType.DiscountNewBook, 0.05 },
+                    { Constants.PriceDetailsType.DiscountNewBookThreshold, 3 },
                 }
             },
             new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = 4,
                 PriceDetails = new Dictionary<string, double>
                 {
                     { Constants.PriceDetailsType.FixedPrice, 0 },
-                    { Constants.PriceDetailsType.DiscountAmount, 0.1 },
-                    { Constants.PriceDetailsType.DiscountThreshold, 3 },
+                    { Constants.PriceDetailsType.DiscountOldBook, 0.1 },
+                    { Constants.PriceDetailsType.DiscountNewBook, 0 },
+                    { Constants.PriceDetailsType.DiscountNewBookThreshold, 0 },
                 }
             },
             new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = 1,
                 PriceDetails = new Dictionary<string, double>
                 {
                    { Constants.PriceDetailsType.FixedPrice, 200 },
-                   { Constants.PriceDetailsType.DiscountAmount, 0.15 },
-                   { Constants.PriceDetailsType.DiscountThreshold, 3 },
+                   { Constants.PriceDetailsType.DiscountOldBook, 1 },
+                   { Constants.PriceDetailsType.DiscountNewBook, 0.15 },
+                   { Constants.PriceDetailsType.DiscountNewBookThreshold, 3 },
                 }
             },
-            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 3,
+            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 2,
                 PriceDetails = new Dictionary<string, double>
                 {
                   { Constants.PriceDetailsType.FixedPrice, 75 },
-                  { Constants.PriceDetailsType.DiscountAmount, 3 },
+                  { Constants.PriceDetailsType.DiscountOldBook, 1 },
+
+                  { Constants.PriceDetailsType.DiscountNewBook, 0.15 },
+                  { Constants.PriceDetailsType.DiscountNewBookThreshold, 3 },
                 }              
             },
-            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 3,
+            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 2,
                 PriceDetails = new Dictionary<string, double>
-                {
-                    { Constants.PriceDetailsType.FixedPrice, 75 },
-                    { Constants.PriceDetailsType.DiscountAmount, 0.15 },
-                    { Constants.PriceDetailsType.DiscountThreshold, 3 },
-                },
+                { { Constants.PriceDetailsType.FixedPrice, 75 }, { Constants.PriceDetailsType.DiscountOldBook, 1 }, { Constants.PriceDetailsType.DiscountNewBook, 0.15 }, { Constants.PriceDetailsType.DiscountNewBookThreshold, 3 }, },
                 BookCategoryId = 1
             },
-            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 3,
+            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 2,
                 PriceDetails = new Dictionary<string, double>
-                {
-                    { Constants.PriceDetailsType.FixedPrice, 75 },
-                    { Constants.PriceDetailsType.DiscountAmount, 0.15 },
-                    { Constants.PriceDetailsType.DiscountThreshold, 3 },
-                },
+                { { Constants.PriceDetailsType.FixedPrice, 75 }, { Constants.PriceDetailsType.DiscountOldBook, 1 }, { Constants.PriceDetailsType.DiscountNewBook, 0.15 }, { Constants.PriceDetailsType.DiscountNewBookThreshold, 3 }, },
                 BookCategoryId = 3
             }
         };
